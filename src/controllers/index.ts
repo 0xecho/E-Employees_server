b40743e97b344a669bf4dd0df9792f9a65aab527
0xecho <0xecho@proto-code.com>
2022-05-26 12:04:42 +0300

    feat: refactors controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+import asyncHandler from "express-async-handler"
+import { IEmployee } from '../models/schema'
+import employeeService from '../services/employeeService'
+
+export const get_employees =  asyncHandler(async (req: Request, res: Response) => {
+    const employees = await employeeService.get_employees()
+    res.send(employees)
+})
+
+export const create_employee = asyncHandler(async (req: Request, res: Response) => {
+    const employee: IEmployee = req.body
+    const created_employee = await employeeService.create_employee(employee)
+    res.send(created_employee)
+})
+
+export const update_employee = asyncHandler(async (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const employee: IEmployee = req.body
+    const updated_employee = await employeeService.update_employee(id, employee)
+    res.send(updated_employee)
+})
+
+export const delete_employee = asyncHandler(async (req: Request, res: Response) => {
+    const id: string = req.params.id
+    const deleted_employee = await employeeService.delete_employee(id)
+    res.send(deleted_employee)
+})
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import asyncHandler from "express-async-handler"
-
-import { EmployeeModel, IEmployee } from './models/schema'
-import employeeService from './services/employeeService'
 import mongoose, { ObjectId } from 'mongoose'
+
+import { 
+    create_employee,
+    get_employees,
+    update_employee,
+    delete_employee 
+} from './controllers/index'
 
 dotenv.config()
 
@@ -20,32 +23,12 @@ app.get('/', (req: Request, res: Response) => {
     res.send("Dube zare yelem, nege temelesu.")
 })
 
-app.get('/employees', asyncHandler(async (req: Request, res: Response) => {
-    const employees = await employeeService.get_employees()
-    res.send(employees)
-}))
-
-app.post('/employees', asyncHandler(async (req: Request, res: Response) => {    
-    const employee: IEmployee = req.body
-    const created_employee = await employeeService.create_employee(employee)
-    res.send(created_employee)
-}))
-
-app.put('/employees/:id', asyncHandler(async (req: Request, res: Response) => {
-    const id: string = req.params.id
-    const employee: IEmployee = req.body
-    const updated_employee = await employeeService.update_employee(id, employee)
-    res.send(updated_employee)
-}))
-
-app.delete('/employees/:id', asyncHandler(async (req: Request, res: Response) => {
-    const id: string = req.params.id
-    const deleted_employee = await employeeService.delete_employee(id)
-    res.send(deleted_employee)
-}))
+app.get('/employees', get_employees)
+app.post('/employees', create_employee)
+app.put('/employees/:id', update_employee)
+app.delete('/employees/:id', delete_employee)
 
 const MONGOOSE_URI: string = process.env.MONGOOSE_URI || "mongodb://localhost:27017/employees"
 mongoose.connect(MONGOOSE_URI)
-
 
 app.listen(port);
